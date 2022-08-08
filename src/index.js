49963e8369ca281bef203c319d3fc2fb269a466d
egstar <borham@hotmail.com>
2022-08-09 00:47:54 +0200

    first test

--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Layout from './Components/Layout';
+import Home from './Components/Pages/Index/index';
+import Login from './Components/Pages/Login/login';
+import Profile from './Components/Pages/Profile/profile';
+import Admin from './Components/Pages/Admin/admin';
+import Quiz from './Components/Pages/Quiz/quiz';
+import Error from './Components/Error';
+import Signup from './Components/Pages/Signup/signup';
 import reportWebVitals from './reportWebVitals';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="error" element={<Error />} />
+          <Route path="admin" element={<Admin />} />
+          <Route path="quiz" element={<Quiz />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
